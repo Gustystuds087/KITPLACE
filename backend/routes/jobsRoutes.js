a814386f959b807c8185f94f75092b4874c3e4ae
Anshul <sanshul087@gmail.com>
2023-12-28 21:40:00 +0530

    major changes

---
+++ backend/routes/jobsRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { createJob, singleJob, updateJob, showJobs, deleteJob } = require('../controllers/jobsController');
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
+
+
+
+//jobs routes
+
+// /api/job/create
+router.post('/job/create', isAuthenticated, isAdmin, createJob);
+// /api/job/id
+router.get('/job/:id', singleJob);
+// /api/job/update/job_id
+router.put('/job/update/:job_id', isAuthenticated, isAdmin, updateJob);
+// /api/job/delete/job_id
+router.delete('/job/delete/:job_id', isAuthenticated, isAdmin, deleteJob);
+// /api/jobs/show
+router.get('/jobs/show', showJobs);
+
+
+
+module.exports = router;
